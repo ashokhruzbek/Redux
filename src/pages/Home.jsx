ff0b7b4eb49a10b47637133be9e299afdf070ab7
shokhruzbek <ashokhruzbek@gmail.com>
2025-05-05 13:20:40 +0500

    some updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 function Home() {
     const students = useSelector(state => state.students);
+    const dispatch = useDispatch();
+
     console.log(students);
   return (
-    <div>Home</div>
+    <div>
+      <h1>Home</h1>
+      <button onClick={()=> dispatch({type: "ADD_STUDENT"})}>ADD Jahongir</button>
+      <button onClick={()=> dispatch({type: "REMOVE_STUDENT"})}>Remove Azamat</button>
+    </div>
   )
 }
 
--- src/redux/studentReducer.js
+++ src/redux/studentReducer.js
@@ -1,8 +1,25 @@
 const boshlangichQiymat = {
     students: ["Azamat", "Shahzodbek", "Shohruh", "Fazliddin"],
+    grades: [1, 3, 5]
 }
 
 
 export const studentReducer = (state= boshlangichQiymat, action) =>{
-    return state;
+    if (action.type === "ADD_STUDENT") {
+        // state ga student qoshamz
+        return {
+            ...state,
+            students: [...state.students, "Jahongir"]
+        }
+    } else if(action.type === "REMOVE_STUDENT"){
+        // Bu yerda statemizdan student olib tashlaymiz
+        return {
+            ...state,
+            students: state.students.filter((student)=> student !== "Azamat")
+        }
+
+
+    } else{
+        return state;
+    }
 }
