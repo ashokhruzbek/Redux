abbb5f378f4fe498b3ec712203f70260a468fec2
shokhruzbek <ashokhruzbek@gmail.com>
2025-05-06 09:29:00 +0500

    some updates

--- src/redux/studentReducer.js
+++ src/redux/studentReducer.js
@@ -1,25 +1,20 @@
 const boshlangichQiymat = {
     students: ["Azamat", "Shahzodbek", "Shohruh", "Fazliddin"],
-    grades: [1, 3, 5]
-}
-
-
-export const studentReducer = (state= boshlangichQiymat, action) =>{
+    grades: [1, 3, 5],
+  };
+  
+  export const studentReducer = (state = boshlangichQiymat, action) => {
     if (action.type === "ADD_STUDENT") {
-        // state ga student qoshamz
-        return {
-            ...state,
-            students: [...state.students, "Jahongir"]
-        }
-    } else if(action.type === "REMOVE_STUDENT"){
-        // Bu yerda statemizdan student olib tashlaymiz
-        return {
-            ...state,
-            students: state.students.filter((student)=> student !== "Azamat")
-        }
-
-
-    } else{
-        return state;
+      return {
+        ...state,
+        students: [...state.students, "Jahongir"],
+      };
+    } else if (action.type === "REMOVE_STUDENT") {
+      return {
+        ...state,
+        students: state.students.filter((student) => student !== "Azamat"),
+      };
+    } else {
+      return state;
     }
-}
+  };
