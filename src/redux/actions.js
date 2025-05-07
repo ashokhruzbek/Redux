b7a31082ea9470c97c7f7ae516e338ab682508f9
shokhruzbek <ashokhruzbek@gmail.com>
2025-05-07 09:42:04 +0500

    i added actions functions

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,7 +1,17 @@
-import { SORT_AGE } from "./actionTypes"
+import { CHANGE_GRADE, MARRY, SORT_AGE } from "./actionTypes"
 
 export const sortStudents = ()=>{
     return {
         type: SORT_AGE
+    }
+}
+export const marryStudents = ()=>{
+    return {
+        type: MARRY
+    }
+}
+export const changeGrades = ()=>{
+    return {
+        type: CHANGE_GRADE
     }
 }
